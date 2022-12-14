49ed83d27215bd324d983543cf10f6e703be65bd
Abhigyan <abhigyanritiz63@gmail.com>
2022-12-14 14:52:21 +0530

    new changes

--- index.php
+++ index.php
@@ -5,20 +5,12 @@
  * 
  * @package Abhigyan's Theme
  */
+
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<div class="content">
+    Content
+</div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Theme</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<?php get_footer();
